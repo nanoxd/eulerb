888df7c1dc4fab4a155619c6cfce1bc46ee9d82b
Fernando Paredes <nanoid.xd@gmail.com>
2013-05-31 09:52:21 -0500

    Futile attempt at problem number 1 (I'm overcomplicating it)

---
+++ problem_1.rb
@@ -0,0 +1,15 @@
+# Multiples of 3 and 5
+# Problem 1
+#
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000
+
+print "Please enter a number"
+amount = gets.chomp!.to_i
+
+range_amount = (1..amount)
+
+range_amount.each do |number|
+
+end
+
