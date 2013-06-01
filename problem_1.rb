f7e28924cf14fc7dfc5894b0d9c47e71fb7972a3
Fernando Paredes <nanoid.xd@gmail.com>
2013-06-01 08:41:50 -0500

    Finish Problem 1         * Utilize Integer#times to reach 1000         * Use an _or_ operator to distinguish whether a number is         * divided by 5 or 3

--- problem_1.rb
+++ problem_1.rb
@@ -4,12 +4,12 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000
 
-print "Please enter a number"
-amount = gets.chomp!.to_i
+sum = 0 # Initial sum
 
-range_amount = (1..amount)
-
-range_amount.each do |number|
-
+# Iterates through 0-999
+# then adds to sum if divisible by 3 or 5 without remainder
+1000.times do |n|
+  sum += n if n % 3 == 0 || n % 5 == 0
 end
 
+puts sum
