96d98ce38cd845a540904347ba0d0e6ffea49983
Fernando Paredes <nano@fdp.io>
2014-05-12 16:55:11 -0700

    :ok_hand: Problem 30
    ruby p30.rb  7.23s user 0.01s system 99% cpu 7.245 total

---
+++ p30.rb
@@ -0,0 +1,20 @@
+# Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+#
+# 1634 = 14 + 64 + 34 + 44
+# 8208 = 84 + 24 + 04 + 84
+# 9474 = 94 + 44 + 74 + 44
+# As 1 = 14 is not a sum it is not included.
+#
+# The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+#
+# Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+
+def sum_of_fifth_power?(num)
+  ary = num.to_s.split("").map { |digit| digit.to_i }
+  sum = ary.inject(0) { |sum, i| sum += i ** 5 }
+  num == sum
+end
+
+total = 0
+(2..1_000_000).each { |i| total += i if sum_of_fifth_power?(i) }
+puts total
