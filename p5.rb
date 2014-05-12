22e3f0dd6ccdae36e8e63a4d188896187e613546
Fernando Paredes <nano@fdp.io>
2014-05-12 11:28:29 -0700

    :ok_hand: Problem 5
    I was originally going to recreate lcm but found it using Dash
    Dash FTW :bomb:

---
+++ p5.rb
@@ -0,0 +1,7 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1
+# to 10 without any remainder. What is the smallest positive number that is 
+# evenly divisible by all of the numbers from 1 to 20?
+
+smallest_number = (1..20).inject(1) { |result, num| result.lcm(num) }
+
+puts smallest_number
